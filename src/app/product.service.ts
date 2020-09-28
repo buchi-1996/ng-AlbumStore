03bd1b003275d9138577018584fb6cd358be9ad9
buchi-1996 <ukaigweonyebuchi@gmail.com>
2020-09-28 08:04:05 +0100

    new

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable'
 import { Album } from './album';
+import { Product } from './product';
 
 import 'rxjs/add/operator/map';
 
@@ -11,10 +12,14 @@ import 'rxjs/add/operator/map';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-
+  private _productsUrl = '../assets/products.json';
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 }
