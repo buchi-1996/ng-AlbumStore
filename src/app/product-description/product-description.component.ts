4dfe136c9fbf2d202e449365fd34172f6132339e
buchi-1996 <ukaigweonyebuchi@gmail.com>
2020-09-28 07:08:00 +0100

    new commit

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -14,7 +14,10 @@ export class ProductDescriptionComponent implements OnInit {
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe((response) => this.albumInfo = response );
+    this._productService.getAlbum(1).subscribe((response) => {
+      console.log(response);
+      return this.albumInfo = response;
+    } );
     
   }
 
