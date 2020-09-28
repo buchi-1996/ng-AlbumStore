3b4372a6e3b8e07d282684c233aedd23e73df8aa
buchi-1996 <ukaigweonyebuchi@gmail.com>
2020-09-28 08:55:01 +0100

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,11 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import {  RouterModule,  Routes } from '@angular/router';
 
-@appRoutes: Routes [
+const appRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
-  { path: 'product/:id', component: ProductPageComponent }
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
 ]
-
 @NgModule({
   declarations: [
     AppComponent,
